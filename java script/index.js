70f90b789302adb799320c755d1b9e3f015a06e1
Amarachi Ojini <ojinihappiness4@gmail.com>
2026-06-28 17:35:04 +0100

    JS Fundamentals: if/else logic and decision making

--- java script/index.js
+++ java script/index.js
@@ -1,1 +1,8 @@
-console.log("Hello Amarachi")
+const tokenName = "AmaraToken"
+let myBalance = 100
+
+console.log("Token:", tokenName)
+console.log("Balance:", myBalance)
+
+myBalance = 250
+console.log("New Balance:", myBalance)
