7bede104b9ba211db3c1e801bb9c04f8666ac04c
Amarachi Ojini <ojinihappiness4@gmail.com>
2026-06-29 21:43:31 +0100

    Organised js-practice folder

--- java script/index.js
+++
@@ -1,8 +0,0 @@
-const tokenName = "AmaraToken"
-let myBalance = 100
-
-console.log("Token:", tokenName)
-console.log("Balance:", myBalance)
-
-myBalance = 250
-console.log("New Balance:", myBalance)
